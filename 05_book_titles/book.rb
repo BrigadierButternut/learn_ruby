439578c5e2700951aaf622959bee718412f5d743
Laura <laurabennett701@gmail.com>
2017-03-22 21:30:17 -0500

    finished the book titles assignment - one more to go!

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,22 +1,29 @@
 class Book
 # write your code here
-attr_reader :title
+
+#the attribute accessor creates an instance variable @title
+#an instance variable is accessible to all methods within a class
+#attribute accessors also create a method called "name=" to set the attribute
+attr_accessor :title
+
 
 #must define the method "title" for use with this class
-def title=(book_title)
-  #splitting the title into seperate words to be dealt with individually
-  title_words = book_title.split(" ")
-  #Always capitalize the first word
-  title_words[0] = title_words[0].capitalize
-  word_number = 1
-  while word_number < title_words.length
-    little_words = ["a" "an" "and" "the" "in" "of"]
-    if little_words.include? title_words[word_number] == false
-      title_words[word_number].capitalize
-    end
-    word_number += 1
-  end
-  title_words.join ' '
-end
+def title
+  little_words = ["in", "the", "or", "of", "and","a","an", "but","for","on","at","from","by"]
+  book_title = @title
+  #split into seperate words
+  title_words = book_title.split
+  #capitalize the first word
+  title_words[0].capitalize!
+  #for every word in the title...
+  title_words.each do |word|
+    #...capitalize that word if and only if it is not a "little word"
+    if !little_words.include? "#{word}"
+      word.capitalize!
+    end #end of if statement
+  end #end of block
+  title_words.join " "
+end #end of title definition
 
-end
+
+end #end of class definition
